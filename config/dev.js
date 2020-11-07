ebce2a9b22a3f64fc9fbd3d63e201a3f33c0a2a4
Josef Macků <josef@macku.cz>
2020-11-07 12:36:12 +0100

    Bug fixed in config

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,5 @@ module.exports = {
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
   mongoURI: 'mongodb://127.0.0.1/blog_dev',
   cookieKey: '123123123',
-  redisUrl = 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379'
 };
